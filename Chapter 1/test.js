1727b9574957cd3b5881d992d9388010e318b4db
gagannn19 <gagansharmaa19@gmail.com>
2026-08-27 09:01:52 +0530

    first commit

---
+++ Chapter 1/test.js
@@ -0,0 +1,15 @@
+"use strict";
+// typescript gives us type safety...
+Object.defineProperty(exports, "__esModule", { value: true });
+var a = 12;
+var b = 13;
+// a = "gagan"
+// TypeScript Setup
+// • Setting up a TypeScript project
+// •Configuring tsconfig. json
+// •CompilingTypeScript
+function abcd() {
+    let x = 12;
+    let y = 22;
+}
+//# sourceMappingURL=test.js.map
---
+++ Chapter 1/test.ts
@@ -0,0 +1,15 @@
+// typescript gives us type safety...
+
+var a = 12;
+var b = 13;
+// a = "gagan"
+
+// TypeScript Setup
+// • Setting up a TypeScript project
+// •Configuring tsconfig. json
+// •CompilingTypeScript
+
+function abcd() {
+  let x = 12;
+  let y = 22;
+}
