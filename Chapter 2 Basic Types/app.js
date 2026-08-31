25eb28901efdcf19d065db5ae43b34d94b67300c
gagannn19 <gagansharmaa19@gmail.com>
2026-08-31 20:29:56 +0530

    chapter 2

--- Chapter 2 Basic Types/app.js
+++ Chapter 2 Basic Types/app.js
@@ -2,7 +2,8 @@
 // Basic Types
 // •Primitive types (number, string, boolean)
 // •Arrays
-// •Tuples // • Enums
+// •Tuples 
+// • Enums
 // •Any, Unknown, Void, Null, Undefined, Never
 Object.defineProperty(exports, "__esModule", { value: true });
 // Data type is divided into 2 parts in TypeScript
@@ -17,5 +18,57 @@ console.log(a);
 // Array → can contain multiple values of the same type, and its length is flexible.
 // Tuple → has a fixed structure, where each position can have a specific type.
 let arr = [1, 2, 3, "gagan", { name: "gagan" }];
-let tup = ["harsh", 22];
+let tup = ["harsh", 22]; // tuples...
+// enumerations
+var UserRoles;
+(function (UserRoles) {
+    UserRoles["ADMIN"] = "admin";
+    UserRoles["GUEST"] = "guest";
+    UserRoles["SUPER_ADMIN"] = "super_admin";
+})(UserRoles || (UserRoles = {}));
+// UserRoles.SUPER_ADMIN
+var StatusCodes;
+(function (StatusCodes) {
+    StatusCodes["ABANDONED"] = "abandoned status code 500";
+    StatusCodes["NOTFOUND"] = "not found status code 404";
+})(StatusCodes || (StatusCodes = {}));
+// StatusCodes.NOTFOUND
+// Any, Unknown, Void, Null, Undefined, Never
+let a2; // declaring variable with type
+a2 = 12;
+// a2 = true;             //  gives us warnig 
+let a3; // its type is "any"    
+// user have to make ure that his variable shouldn't of "any" data type... then what is the motive of using typescript.
+a3 = 55;
+a3 = "gagan";
+// unkown
+let a4;
+a4 = 99;
+a4 = "hahan";
+if (typeof a4 === "string") {
+    a4.toUpperCase();
+}
+// void
+function abcd() {
+    console.log("hey");
+}
+function efgh() {
+    return "gagan";
+}
+// null
+let a5;
+// a = 12;        // it give us error
+let a6;
+a6 = null;
+a6 = "fghj";
+// a6 = 88;           // it gives error
+// undefined (same undefined as javascript, same logic... if we didnt give a value to a variable then that variable is undefined)
+let a7;
+// never
+function ijkl() {
+    while (true) {
+    }
+}
+ijkl();
+console.log("hey");
 //# sourceMappingURL=app.js.map
--- Chapter 2 Basic Types/app.ts
+++ Chapter 2 Basic Types/app.ts
@@ -1,7 +1,8 @@
 // Basic Types
 // •Primitive types (number, string, boolean)
 // •Arrays
-// •Tuples // • Enums
+// •Tuples 
+// • Enums
 // •Any, Unknown, Void, Null, Undefined, Never
 
 // Data type is divided into 2 parts in TypeScript
@@ -20,4 +21,108 @@ console.log(a);
 // Array → can contain multiple values of the same type, and its length is flexible.
 // Tuple → has a fixed structure, where each position can have a specific type.
 let arr = [1,2,3, "gagan", {name : "gagan"}];
-let tup: [string, number] = ["harsh", 22] 
+let tup: [string, number] = ["harsh", 22]; // tuples...
+
+
+
+
+
+
+
+// enumerations
+
+enum UserRoles{
+  ADMIN = "admin",
+  GUEST = "guest",
+  SUPER_ADMIN = "super_admin"
+}
+
+// UserRoles.SUPER_ADMIN
+
+enum StatusCodes {
+  ABANDONED = "abandoned status code 500",
+  NOTFOUND = "not found status code 404"
+}
+
+// StatusCodes.NOTFOUND
+
+
+
+
+
+
+
+
+// Any, Unknown, Void, Null, Undefined, Never
+
+let a2 : number;          // declaring variable with type
+
+a2 = 12;
+// a2 = true;             //  gives us warnig 
+
+
+let a3;            // its type is "any"    
+// user have to make ure that his variable shouldn't of "any" data type... then what is the motive of using typescript.
+
+a3 = 55;
+a3 = "gagan"; 
+
+
+
+// unkown
+
+let a4 : unknown;
+a4 = 99;
+a4 = "hahan";
+
+if (typeof a4 === "string") {
+  a4.toUpperCase();
+}
+
+
+
+
+// void
+
+function abcd(): void {
+  console.log("hey");
+}
+
+function efgh() : string {
+  return "gagan"
+}
+
+
+
+
+// null
+
+let a5 : null;
+// a = 12;        // it give us error
+
+
+let a6 : null | string;
+a6 = null;
+a6 = "fghj";
+// a6 = 88;           // it gives error
+
+
+
+
+
+// undefined (same undefined as javascript, same logic... if we didnt give a value to a variable then that variable is undefined)
+
+let a7 : undefined;
+
+
+
+// never
+
+function ijkl() : never  { //(never means it returns nothing)
+  while(true){
+
+  }
+}
+
+ijkl();
+console.log("hey");
