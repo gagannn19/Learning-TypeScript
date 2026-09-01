610d7df0ce116402ff612b8c31bbfcb154657020
gagannn19 <gagansharmaa19@gmail.com>
2026-09-01 23:04:01 +0530

    chapter 3

--- Chapter 3 Type Inference, Type Annotation/app.ts
+++ Chapter 3 Type Inference, Type Annotation/app.ts
@@ -3,8 +3,23 @@
 // --> Type Annotations
 
 let a = 12;
-// in above code we didnt define thw type of the variable and data, typescript by itself find outs its variable type , this is called type inference.
+// in above code we didnt define the type of the variable and data, typescript by itself find outs its variable type , this is called type inference.
 
 let a1 = "12";
 
 let a3 : string = "gagan"; // this is type annotations
+
+let a4 : number | boolean | string;
+
+a4 = "gagan";
+a4 = true;
+a4 = 33;
+
+
+
+// type annotation
+
+function abcd (a:string, b:number):void { 
+
+}
+
